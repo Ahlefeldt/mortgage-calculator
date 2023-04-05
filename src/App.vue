5fcbcce0209f9b071a2ff07ddbaedcbb6ab3573c
Nicolai Ahlefeldt <ahlefeldtnicolai@gmail.com>
2023-04-05 17:18:40 +0200

    feat: more responsive padding

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import BaseCard from '@/components/TheCalculatorForm.vue'
 </script>
 
 <template>
-  <div class="flex justify-center min-h-screen w-screen p-12">
+  <div class="flex justify-center min-h-screen w-screen p-6 sm:p-12">
     <div class="w-full max-w-2xl">
       <h1 class="text-3xl font-bold text-gray-100 mb-6">Realkredit Udgiftsberegner</h1>
       <BaseCard />
