4b26d7870ef65811c1a7652aee2b95888cfb0a2e
Nicolai Ahlefeldt <ahlefeldtnicolai@gmail.com>
2023-04-06 10:27:52 +0200

    feat: add scroll for BaseSelect

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ChevronDownIcon as HeroIconChevronDown, CheckIcon as HeroIconCheck } from '@heroicons/vue/20/solid'
+import { ChevronDownIcon, CheckIcon } from '@heroicons/vue/20/solid'
 import { defineProps, ref } from 'vue'
 
 const props = defineProps<{
@@ -23,7 +23,6 @@ const onChange = () => {
   emit('update:modelValue', selectedValue.value)
 }
 </script>
-
 <template>
   <div class="flex flex-col w-full">
     <label id="listbox-label" class="text-gray-400 text-sm font-medium leading-6">{{ label }}</label>
@@ -35,15 +34,15 @@ const onChange = () => {
       >
         <span class="block truncate">{{ options.find(option => option.value === selectedValue)?.label }}</span>
         <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-          <HeroIconChevronDown class="h-5 w-5 text-gray-300" />
+          <ChevronDownIcon class="h-5 w-5 text-gray-300" />
         </span>
       </button>
       <div v-if="isOpen" class="origin-top-right absolute right-0 mt-2 w-full rounded-md shadow-lg bg-gray-700 z-10">
-        <ul class="py-1 leading-6 text-white text-sm">
+        <ul class="py-1 leading-6 text-white text-sm max-h-[200px] overflow-auto">
           <li v-for="option in options" :key="option.value" class="cursor-default select-none relative py-2 pl-3 pr-9 hover:bg-indigo-500 hover:text-white" @click="onSelect(option.value)">
             <span class="font-normal block truncate">{{ option.label }}</span>
             <span v-if="selectedValue === option.value" class="text-indigo-300 absolute inset-y-0 right-0 flex items-center pr-4">
-              <HeroIconCheck class="h-5 w-5" />
+              <CheckIcon class="h-5 w-5" />
             </span>
           </li>
         </ul>
