fc662294256d1cb42702f3946e300df81c7aaffc
Nicolai Ahlefeldt <ahlefeldtnicolai@gmail.com>
2023-04-06 22:21:21 +0200

    refactor: update naming for variables

--- src/components/TheCalculatorForm.vue
+++ src/components/TheCalculatorForm.vue
@@ -10,8 +10,8 @@ import CalculatorResultRow from '@/components/CalculatorResultRow.vue'
 import CalculatorTermSelector from '@/components/CalculatorTermSelector.vue'
 
 const gracePeriod = ref(true)
-const years = ref<string>('30')
-const months = ref<string>('0')
+const yearsRemaining = ref<string>('30')
+const monthsRemaining = ref<string>('0')
 const rateType = ref<string>('fixed')
 const rateTypes = ref<RadioItems[]>([
   { value: 'variable', label: 'Variabel rente', description: 'Du betaler en variabel rente, der kan ændre sig over tid' },
@@ -22,15 +22,15 @@ const interestRate = ref<number>(0)
 const futureInterestRate = ref<number>(0)
 const adminFee = ref<number>(0)
 
-const calculateMortgagePayment = (interestRate: number, adminFee: number, mortgageAmount: number) => {
+const calculateMortgagePayment = (interestRate: number) => {
   const interest = interestRate / 100
-  const fee = adminFee / 100
-  return (interest + fee) * mortgageAmount
+  const fee = adminFee.value / 100
+  return (interest + fee) * mortgageAmount.value
 }
 
-const paymentYearly = computed<number>(() => calculateMortgagePayment(interestRate.value, adminFee.value, mortgageAmount.value))
+const paymentYearly = computed<number>(() => calculateMortgagePayment(interestRate.value))
 const paymentMonthly = computed<number>(() => paymentYearly.value / 12)
-const futurePaymentYearly = computed<number>(() => calculateMortgagePayment(futureInterestRate.value, adminFee.value, mortgageAmount.value))
+const futurePaymentYearly = computed<number>(() => calculateMortgagePayment(futureInterestRate.value))
 const futurePaymentMonthly = computed<number>(() => futurePaymentYearly.value / 12)
 const yearlyDifference = computed<number>(() => futurePaymentYearly.value - paymentYearly.value)
 const monthlyDifference = computed<number>(() => futurePaymentMonthly.value - paymentMonthly.value)
@@ -38,8 +38,8 @@ const monthlyDifference = computed<number>(() => futurePaymentMonthly.value - pa
 
 <template>
   <div class="bg-gray-800 rounded-lg shadow-lg overflow-hidden p-6" data-testid="calculator-form">
-    <BaseToggle v-model="gracePeriod" label="Afdragsfrihed" description="Angiv, om din realkredit har en afdragsfri periode" />
-    <CalculatorTermSelector v-if="!gracePeriod" class="mt-6 pt-6 border-t border-gray-700" v-model:years="years" v-model:months="months" />
+    <BaseToggle v-model="gracePeriod" label="Afdragsfrihed" description="Angiv, om din realkredit er i en afdragsfri periode" />
+    <CalculatorTermSelector v-if="!gracePeriod" class="mt-6 pt-6 border-t border-gray-700" v-model:years="yearsRemaining" v-model:months="monthsRemaining" />
     <BaseRadioGroup class="mt-6 pt-6 border-t border-gray-700" v-model="rateType" :items="rateTypes" />
     <BaseInput class="mt-6 pt-6 border-t border-gray-700" v-model="mortgageAmount" type="number" step="1" label="Restgæld" unit="kr" />
     <BaseInput
@@ -52,11 +52,8 @@ const monthlyDifference = computed<number>(() => futurePaymentMonthly.value - pa
     />
     <BaseInput v-model="interestRate" type="number" step="0.01" label="Rentesats" unit="%" />
     <BaseInput v-if="rateType === 'variable'" v-model="futureInterestRate" type="number" step="0.01" label="Fremtidig Rentesats" unit="%" />
-    <CalculatorResultRow class="mt-6 pt-6 border-t border-gray-700" label="Årlig udgift" :result="paymentYearly" />
-    <CalculatorResultRow class="mt-1" label="Månedlig udgift" :result="paymentMonthly" />
-    <CalculatorResultRow v-if="rateType === 'variable'" class="mt-6" label="Fremtidig årlig udgift" :result="futurePaymentYearly" />
+    <CalculatorResultRow class="mt-6 pt-6 border-t border-gray-700" label="Månedlig udgift" :result="paymentMonthly" />
     <CalculatorResultRow v-if="rateType === 'variable'" class="mt-1" label="Fremtidig månedlig udgift" :result="futurePaymentMonthly" />
-    <CalculatorResultRow v-if="rateType === 'variable'" class="mt-6" label="Årlig difference" :result="yearlyDifference" :colorize="true" />
     <CalculatorResultRow v-if="rateType === 'variable'" class="mt-1" label="Månedlig difference" :result="monthlyDifference" :colorize="true" />
   </div>
 </template>
