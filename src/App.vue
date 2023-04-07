0c47458633eac0416f19b3c655e9d8e95ae32af6
Nicolai Ahlefeldt <ahlefeldtnicolai@gmail.com>
2023-04-07 10:36:39 +0200

    refactor: rename component to be more semantic

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
-import BaseCard from '@/components/TheCalculatorForm.vue'
+import TheCalculatorForm from '@/components/TheCalculatorForm.vue'
 </script>
 
 <template>
   <div class="flex justify-center min-h-screen w-screen px-6 py-4 sm:px-12 sm:py-8">
     <div class="w-full max-w-2xl">
       <h1 class="text-2xl sm:text-3xl font-bold text-gray-100 mb-4 sm:mb-6">Realkredit Udgiftsberegner</h1>
-      <BaseCard />
+      <TheCalculatorForm />
     </div>
   </div>
 </template>
-
-<style scoped></style>
